b16afd5dc397b517b6fb34c65db519213e60bcef
CEIBA\sebastian.vasquez <sebastian.vasquez@ceiba.com.co>
2018-05-11 08:17:49 -0500

    Base

---
+++ src/main/java/com/example/backendceibaparkingapp/BackendceibaparkingappApplication.java
@@ -0,0 +1,12 @@
+package com.example.backendceibaparkingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendceibaparkingappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendceibaparkingappApplication.class, args);
+	}
+}
